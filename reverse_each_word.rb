99b03997ebd281d2fb9890cdabd53bdbf2fac043
Billie Simmons <billiesnyc@gmail.com>
2018-08-16 09:52:05 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,9 @@
+def reverse_each_word(string)
+  final_array = []
+  split_array = string.split
+   split_array.collect do |word|
+    final_array.push(word.split(//).reverse.push(" "))
+    
+  end
+ final_array.join().chop
+end 
