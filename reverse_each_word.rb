f7cd1639fc728d9a532fcb3262134c19ea1490c7
Billie Simmons <billiesnyc@gmail.com>
2018-08-16 09:46:32 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   final_array = []
   split_array = string.split
    split_array.collect do |word|
-    final_array.push(word.split(//).reverse.push(" "))
+    final_array.push(word.split(//).reverse.push(" ").pop)
   end
  final_array.join()
 end 
